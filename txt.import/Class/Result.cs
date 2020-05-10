59ba9372edfc72076db78a5ce1fd1841bd2e8da8
eclipse <eclipse@DESKTOP-J4KSNQ6>
2020-05-10 16:56:19 +0700

    field

--- txt.import/Class/Result.cs
+++ txt.import/Class/Result.cs
@@ -6,6 +6,25 @@ namespace txt.import.Class
 {
     public class Result
     {
+
+        
+        
+
+
+
+
+
+
+
+
+        public string i_highvoltage_id { get; set; }
+        public string c_test_voltage { get; set; }
+        public string c_test_current { get; set; }
+        public string c_test_time { get; set; }
+        public string d_highvoltage { get; set; }
+        public string i_pass { get; set; }
         public string c_serial { get; set; }
+        public string c_series { get; set; }
+        public string c_computer { get; set; }
     }
 }
--- txt.import/ImportSvc.cs
+++ txt.import/ImportSvc.cs
@@ -27,7 +27,16 @@ namespace txt.import
                 var data = line.Split(',');
                 list_reult.Add(new Result()
                 {
+                    //i_highvoltage_id = data[0],
+                    c_test_voltage = data[17],
+                    c_test_current = data[18],
+                    c_test_time = data[20],
+                    d_highvoltage = data[7],
+                    i_pass = data[2],
                     c_serial = data[0],
+                    c_series = data[1],
+                    c_computer = ""
+
                 });
             }
             return list_reult;
@@ -39,7 +48,8 @@ namespace txt.import
             using var con = new MySqlConnection(cs);
             con.Open();
 
-            var sql = "INSERT INTO `b_highvoltage` (`i_highvoltage_id`, `c_test_voltage`, `c_test_current`, `c_test_time`, `d_highvoltage`, `i_pass`, `c_serial`, `c_series`, `c_computer`) VALUES ('1', '123', '123', '123', '12312', '3213213', '213213', '21321', '3123123');";
+            var sql = "INSERT INTO `b_highvoltage` (`i_highvoltage_id`, `c_test_voltage`, `c_test_current`, `c_test_time`, `d_highvoltage`, `i_pass`, `c_serial`, `c_series`, `c_computer`) " +
+                "VALUES ('1', '123', '123', '123', '12312', '3213213', '213213', '21321', '3123123');";
 
             using var cmd = new MySqlCommand(sql, con);
 
