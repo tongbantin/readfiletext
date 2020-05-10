c4e4dbb7a9be51b73655842888c4f9c9d45363ff
Chawakorn <tongbantin@hotmail.com>
2020-05-10 15:16:36 +0700

    Add project files.

---
+++ txt.import/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace txt.import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
