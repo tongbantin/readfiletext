73a2fde64738c834fecdf881deafae1883fe3aae
Chawakorn <tongbantin@hotmail.com>
2020-05-10 15:23:02 +0700

    xx

--- txt.import/Program.cs
+++ txt.import/Program.cs
@@ -7,6 +7,7 @@ namespace txt.import
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!2");
         }
     }
 }
