dd241989de84e957875e1dcf706a441dd6b198da
Chawakorn <tongbantin@hotmail.com>
2020-05-10 16:55:42 +0700

    sql

--- txt.import/ImportSvc.cs
+++ txt.import/ImportSvc.cs
@@ -3,6 +3,7 @@ using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using txt.import.Class;
 
@@ -20,32 +21,58 @@ namespace txt.import
         }
         public virtual List<Result> ReadFile()
         {
-            var lines = File.ReadAllLines(Path.Combine(System.IO.Directory.GetParent(@"./").FullName, "6-1-2020_PASS.txt"));
-            var list_reult = new List<Result>();
-            foreach (var line in lines)
+            try
             {
-                var data = line.Split(',');
-                list_reult.Add(new Result()
+                var lines = File.ReadAllLines(Path.Combine(System.IO.Directory.GetParent(@"./").FullName, "6-1-2020_PASS.txt"));
+                var list_reult = new List<Result>();
+                foreach (var line in lines)
                 {
-                    c_serial = data[0],
-                });
+                    var data = line.Split(',');
+                    list_reult.Add(new Result()
+                    {
+                        c_serial = data[0],
+                    });
+                }
+                return list_reult;
             }
-            return list_reult;
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
         }
         public virtual void AddToDB(List<Result> data)
         {
-            var cs = "Server=localhost;Port=3306;Database=store;User Id=root;";
+            try
+            {
+                var cs = "Server=localhost;Port=3306;Database=store;User Id=root;";
 
-            using var con = new MySqlConnection(cs);
-            con.Open();
+                using var con = new MySqlConnection(cs);
+                con.Open();
 
-            var sql = "INSERT INTO `b_highvoltage` (`i_highvoltage_id`, `c_test_voltage`, `c_test_current`, `c_test_time`, `d_highvoltage`, `i_pass`, `c_serial`, `c_series`, `c_computer`) VALUES ('1', '123', '123', '123', '12312', '3213213', '213213', '21321', '3123123');";
+                var sql = "INSERT INTO `b_highvoltage` ( `c_test_voltage`, `c_test_current`, `c_test_time`, `d_highvoltage`, `i_pass`, `c_serial`, `c_series`, `c_computer`) VALUES ;";
+                foreach (var item in data)
+                {
+                    var row = String.Format(@"('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}')", item.c_serial, item.c_serial, item.c_serial, item.c_serial, item.c_serial, item.c_serial, item.c_serial, item.c_serial, item.c_serial);
+                    sql += row;
+                    if (item != data.LastOrDefault())
+                    {
+                        sql += ',';
+                    }
+                }
+                using var cmd = new MySqlCommand(sql, con);
 
-            using var cmd = new MySqlCommand(sql, con);
+                var version = cmd.ExecuteReader();
 
-            var version = cmd.ExecuteReader();
+                //Console.WriteLine($"PostgreSQL version: {version}");
+            }
+            catch (Exception ex)
+            {
 
-            //Console.WriteLine($"PostgreSQL version: {version}");
+                throw ex;
+            }
+            
         }
     }
 }
